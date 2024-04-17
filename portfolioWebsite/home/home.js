230f4604f3be8c11cae32ec7bc6b48ef734a5e43
erika grossman <erikaagrossmann@gmail.com>
2024-04-17 16:04:00 -0400

    adding to final website

---
+++ portfolioWebsite/home/home.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    this.getElementById('profile').addEventListener('click', function () {
+    const swirls= getElementById('swirls')
+    swirls=== false
+    
+    if (swirls===false) {
+        swirls=== true
+    } else {
+        swirls===false
+    }
+    })
+  })
