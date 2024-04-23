8553a5fe6876548c832657469b4011435ee601e8
erika grossman <erikaagrossmann@gmail.com>
2024-04-23 15:35:13 -0400

    classwork from 4.23

--- portfolioWebsite/home/home.js
+++ portfolioWebsite/home/home.js
@@ -1,12 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
-    this.getElementById('profile').addEventListener('click', function () {
-    const swirls= getElementById('swirls')
-    swirls=== false
+    //gsap.from('body', 1.5, {opacity:0})
+    gsap.from('nav', 1, {right:-500, delay:0.5})
+    gsap.from('p', 1, {opacity:0, delay:0.5})
+    gsap.from('.profile', 1, {opacity:0, delay:0.75})
+
+    // this.getElementById('profile').addEventListener('click', function () {
+     const profile= getElementById('profile')
+    // swirls=== false
     
-    if (swirls===false) {
-        swirls=== true
-    } else {
-        swirls===false
-    }
-    })
+    // if (swirls===false) {
+    //     swirls=== true
+    // } else {
+    //     swirls===false
+    // }
+    // })
+
+    
+        profile.addEventListener('mouseenter',function () {
+          gsap.to(this, {className: 'animation active', duration: 1})} )
+      
+      //remove on mouse leave
+    
+        profile.addEventListener('mouseleave',function () {
+          gsap.to(this, {className: 'animation', duration: 1})} )
+    
+
   })
