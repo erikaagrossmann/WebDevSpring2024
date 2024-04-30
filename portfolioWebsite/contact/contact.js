97a469f58a04710129006125f4d96c151cbcb3ee
erika grossman <erikaagrossmann@gmail.com>
2024-04-30 16:44:00 -0400

    fixing media queries

--- portfolioWebsite/contact/contact.js
+++ portfolioWebsite/contact/contact.js
@@ -1,28 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
-    // const phone= document.getElementById('phone')
-
-   
- 
-    // // phone.addEventListener('mouseenter', function () {
-    // //     gsap.to(this, {outcomeP, duration:1})
-    // // })
-    // // phone.addEventListener('mouseleave', function () {
-    // //     gsap.to(this, {byeP, duration:1})
-    // // })
-    
-    // const email= document.getElementById('email').addEventListener('click', outcomeE)
-    // // const insta= document.getElementById('insta').addEventListener('click', outcomeI)
-    //  const linkedIN= document.getElementById('linkedIn').addEventListener('click', outcomeL)
 
      gsap.from('nav', 1, {right:-500, delay:0.5})
      gsap.from('h2', 1, {opacity:0, delay:0.5})
      gsap.from('.pic', 1, {opacity:0, delay:0.5})
      gsap.from('.container', 1, {opacity:0, delay:0.5})
+
+   
  
   })
 
 
-  
+
 
 
     
