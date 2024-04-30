07a800009f21e64822a6878e3589e84a3231210e
erika grossman <erikaagrossmann@gmail.com>
2024-04-30 14:50:33 -0400

    working on final +contact form exercise

--- portfolioWebsite/home/home.js
+++ portfolioWebsite/home/home.js
@@ -2,16 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
     gsap.from('nav', 1, {right:-500, delay:0.5})
     gsap.from('p', 1, {opacity:0, delay:0.5})
     gsap.from('.profile', 1, {opacity:0, delay:0.5})
-     gsap.from('.animation', 1, {opacity:0, delay:2})
+    gsap.from('.animation', 1, {opacity:0, delay:1.5})
 
-     const swirls= document.getElementById('swirls').addEventListener('click', function() {
+     const swirls= document.getElementById('profile').addEventListener('click', function() {
 
      var animation = document.getElementById("swirls");
-     if (animation.style.display === "none") {
-       animation.style.display = "block";
-     } else {
-       animation.style.display = "none";
-     }
+      animation.classList.toggle('hidden')
     })
 
 
