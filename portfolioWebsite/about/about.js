bcc73f2c4a9e4d527f6d832a88118a963394e7ed
erika grossman <erikaagrossmann@gmail.com>
2024-05-01 13:44:15 -0400

    adding about animation on final

--- portfolioWebsite/about/about.js
+++ portfolioWebsite/about/about.js
@@ -3,5 +3,13 @@ document.addEventListener('DOMContentLoaded', function(){
     gsap.from('h2', 1, {opacity:0, delay:0.5})
     gsap.from('p', 1, {opacity:0, delay:0.5})
     gsap.from('.pic', 1, {opacity:0, delay:0.5})
+    gsap.from('.animation', 1, {opacity:0, delay:1.5})
+
+
+    const fountain= document.getElementById('pic').addEventListener('click', function() {
+
+      var animation = document.getElementById("fountain");
+       animation.classList.toggle('hidden')
+     })
 
   })
